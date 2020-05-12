cb9968c02ce19e2a7e1f5ba9570b10cc192bd318
Fany Menendez Freije <fany@Fanys-MacBook-Pro.local>
2020-05-12 11:44:48 +0200

    port

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// se importa express
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
@@ -5,25 +6,25 @@ const cors = require('cors');
 // crear el servidor
 const app = express();
 
-// Conectar a la base de datos
+// conectar a la base de datos
 conectarDB();
 
-// habilitar cors
+// Habilitar cors
 app.use(cors());
 
 // Habilitar express.json
-app.use( express.json({ extended: true }));
+app.use(express.json({extended: true}));
 
-// puerto de la app
-const port = process.env.port || 4000;
+// puerto del servidor
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
-app.use('/api/usuarios', require('./routes/usuarios'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/proyectos', require('./routes/proyectos'));
-app.use('/api/tareas', require('./routes/tareas'));
+app.use('/api/usuarios',require('./routes/usuarios'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/proyectos',require('./routes/proyecto'));
+app.use('/api/tareas',require('./routes/tareas'));
 
-// arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
-});
+// arrancar el servidor
+app.listen(PORT, (
+    console.log(`El servidor está funcionando en el puerto ${PORT}`)
+))
